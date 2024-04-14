dfff146e2b70a5a19297b12b8e3a9a5480fa6f00
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2024-04-14 17:07:06 -0400

    posts created

--- routes/Create.jsx
+++ routes/Create.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
+import "./Create.css";
+import { supabase } from "../src/client";
 
 const Create = () => {
+    const [post, setPost] = useState({title:"", author:"", content:"", img:""});
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setPost((prev) => {
+            return{
+                ...prev, [name]:value
+            }
+        });
+    }
+
+    const createPost = async(event) => {
+        event.preventDefault();
+
+        await supabase.from("AnimeForums").insert({
+            title: post.title,
+            author: post.author,
+            content: post.content,
+            img:post.img
+        }).select();
+
+        window.location = "/";
+    }
+
     return(
-        <div>
-            
+        <div className="form-page">
+            <h1>Create a new post!</h1>
+            <form>
+                <input type="text" id="title" name="title" placeholder="Title" onChange={handleChange}></input>
+                <br/>
+                <input type="text" id="author" name="author" placeholder="Your Name" onChange={handleChange}></input>
+                <br/>
+                <input type="text" id="content" name="content" placeholder="Content (optional)" onChange={handleChange}></input>
+                <br/>
+                <input type="text" id="img-url" name="img" placeholder="ImageURL (optional" onChange={handleChange}></input>
+                <br/>
+                <button onClick={createPost}>Create Post</button>
+            </form>
         </div>
     );
 };
---
+++ src/components/Feed.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import { supabase } from "../client";
+import "./Feed.css";
+
+const Feed = () => {
+    return(
+        <div className="feed">
+
+        </div>
+    );
+};
+
+export default Feed;
