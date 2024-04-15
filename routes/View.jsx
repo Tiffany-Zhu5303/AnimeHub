b860c1ee0839d06bb93d75c4541e4f1e4bd8e530
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2024-04-14 20:45:35 -0400

    update partial set up and delete

--- routes/View.jsx
+++ routes/View.jsx
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from "react";
 import "./View.css"
 import { supabase } from "../src/client";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const View = () => {
     const {id} = useParams();
     const [count, setCount] = useState(0);
-    const [post, setPost] = useState({created_by:"", title:"", author:"", upvotes:"", img:"", content:"", upvotes:0, comments:[]});
+    const [post, setPost] = useState({id:-1, created_by:"", title:"", author:"", upvotes:"", img:"", content:"", upvotes:0, comments:[]});
 
     useEffect(() => {
         const fetchPost = async() => {
@@ -27,11 +28,27 @@ const View = () => {
         setCount(count+1);
     }
 
+    const deletePost = async(event) => {
+        event.preventDefault();
+
+        await supabase.from('AnimeForums').delete().eq('id', id);
+        window.location = "/";
+    }
+
     return(
         <div className="post-detail">
-            <h4>Posted {post.created_by}</h4>
-            <h4>By: {post.author}</h4>
-            {count === 1 ? <h4>{count} upvote</h4> : <h4>{count} upvotes</h4>}
+            <div className="info">
+                <div className="displayed-info">
+                    <h4>Posted {post.created_by}</h4>
+                    <h4>By: {post.author}</h4>
+                    {count === 1 ? <h4>{count} upvote</h4> : <h4>{count} upvotes</h4>}  
+                </div>
+                <div className="post-editing">
+                    <Link to={"/edit/"+post.id}><img id="edit-icon" className="icons" src="../src/assets/edit-icon.png" /></Link>
+                    <img id="delete-icon" className="icons" src="../src/assets/delete-icon.png" onClick={deletePost}/>
+                </div>
+            </div>
+            
 
             <div className="content">
                 <h2 id="post-title">{post.title}</h2>
