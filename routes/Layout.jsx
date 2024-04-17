c415a111d273b042613da9bd243af8a6cc893034
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2024-04-17 13:20:51 -0400

    targeted search and styling

--- routes/Layout.jsx
+++ routes/Layout.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
+    const sendSearch = (e) => {
+        if(e.key == "Enter"){
+            console.log(e.target.value);
+        }
+        
+    }
+
     return(
         <div className="home">
             <nav className="nav">
                 <h2 id="page-title">Animehub</h2>
-                <input id="search" placeholder="Search Title..."></input>
+                <input id="search" placeholder="Search Title..." onKeyDown={sendSearch}></input>
                 <div className="links">
                     <Link className="nav-link" to="/">Home</Link>
                     <Link className="nav-link" to="/create">Create</Link>
